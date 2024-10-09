424025a08c65a164ecc730c89d333984b229a30a
Tara Ramanathan <tramanat@lion.lmu.edu>
2024-10-09 13:03:56 -0700

    create todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import './App.css'
+import { useState } from 'react';
+import TodoList from './TodoList'
 
-function App() {
-
+export default function App() {
+    const [todos, setTodos] = useState([
+        { id: 1, content: 'Meet Masao for Udon' },
+        { id: 2, content: 'Fix up these horrible React notes' },
+    ]);
+    function deleteTodo(id) {
+        setTodos(todos => todos.filter(todo => todo.id !== id));
+    }
     return (
-        <>
-            <h1> To Do list </h1>
-        </>
-    )
+        <div className="App">
+            <h1>My Tasks</h1>
+            <TodoList todos={todos} deleter={deleteTodo} />
+        </div>
+    );
 }
-
-export default App
